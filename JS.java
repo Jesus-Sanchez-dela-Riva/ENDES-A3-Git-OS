7acf9034cb8231e866308e04f638c2076bfc2bb8
Jesus Sanchez de la Riva <sanchezdelariva.jesus@jgvaldemora.org>
2025-04-01 10:03:43 +0200

    He añadido otro ejercicio de programacion para poder hacer el trabajo

--- JS.java
+++ JS.java
@@ -1,10 +1,19 @@
 //Jesús Sánchez de la Riva
 package unidad3;
-public class U03_A1 {
-     public static void main (String[] args){
+import java.util.Scanner;//Importo el Scanner
+public class U03_A2 {//Creo la clase
+    public static void main (String[] args){
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println ("Hola,elige un numero decimal");
+        double numero = sc.nextDouble();//Indico la variable
+        System.out.println ("Hola, has elegido " + numero); 
+        System.out.println ("Hola, elige un numero entero");
+        int entero = sc.nextInt();//Indico la variable
+        System.out.println ("Hola, has elegido " + entero); 
+        System.out.println ("Hola, elige un numero escrito");
+        String numero3 =  sc.next();     
+        System.out.println ("Hola, has elegido " + numero3);
         
-         System.out.println ("Hola. Encantado de conocerlo");
-         System.out.println("Jesús Sánchez de la Riva");
-         System.out.println("Estoy editando esto gracias al comando nano")
-	} 
+    }
 }
